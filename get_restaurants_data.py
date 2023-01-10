d99f7367ac2cdf081cf5a87fffa0bf875ea00c14
Waseem Shammut <wshammout@gmail.com>
2023-01-11 00:36:13 +0300

    fixed data grab script

--- get_restaurants_data.py
+++ get_restaurants_data.py
@@ -6,7 +6,7 @@ import pyodbc
 conn = pyodbc.connect('Driver={SQL Server};Server=DESKTOP-PDC6CGI\SQLEXPRESS;Database=testdb;Trusted_Connection=yes;')
 cursor = conn.cursor()
 
-cursor.execute('Select * from talabat_restaurants_urls where done is null')
+cursor.execute('Select * from talabat_restaurants_urls where page_active is null')
 c = 0
 
 now = datetime.now()
@@ -29,91 +29,113 @@ for row in cursor.fetchall():
     soup = Soup(d.content, 'html.parser')
 
     html_element = soup.findAll('h1')
-    text = 'h1'
-    for i, line in enumerate(html_element):
-            intStart = str(line).find('data-test="brand-name">') + len('data-test="brand-name">')
-            line_str = str(line)
-            line_str = line_str[intStart:]
-            intEnd = line_str.find('</')
-            entry_name = line_str[:intEnd]
-            #print(entry_name)
-
-    html_element = soup.findAll('p')
-    text = 'cuisine-string white'
+    text = 'Page Not Found'
+    page_not_found = 0
     for i, line in enumerate(html_element):
         if str(line).find(text) > 1:
-            intStart = str(line).find('"brand-cusine">') + len('"brand-cusine">')
-            line_str = str(line)
-            line_str = line_str[intStart:]
-            intEnd = line_str.find('</p')
-            category_name = line_str[:intEnd]
-            #print(category_name)
-
-    html_element = soup.findAll('div')
-    for i, line in enumerate(html_element):
-        if i == 35:
-            intStart = str(line).find('number">') + len('number">')
-            line_str = str(line)
-            line_str = line_str[intStart:]
-            intEnd = line_str.find('</div>')
-            rating = line_str[:intEnd]
-            if len(rating) == 0:
-                rating = 0
+            update_command = 'Update talabat_restaurants_urls Set [page_active] = 0 where id = ' + str(row[0])
+            cursor.execute(update_command)
+            conn.commit()
+            page_not_found = 1
             break
-
-    html_element = soup.findAll('div')
-    text = '"brand-total-ratings">( '
-    for i, line in enumerate(html_element):
-        if str(line).find(text) > 1:
-            intStart = str(line).find('"brand-total-ratings">( ') + len('"brand-total-ratings">( ')
-            line_str = str(line)
-            line_str = line_str[intStart:]
-            intEnd = line_str.find(' Ratings')
-            rated = line_str[:intEnd]
-            rated = line_str[:intEnd]
         else:
-            rated = 0
-        break
+            page_not_found = 0
+            continue
 
-    html_element = soup.findAll('span')
-    text = 'brand-total-reviews">'
-    for i, line in enumerate(html_element):
-        if str(line).find(text) > 1:
-            intStart = str(line).find('reviews">') + len('reviews">')
-            line_str = str(line)
-            line_str = line_str[intStart:]
-            intEnd = line_str.find(' Reviews')
-            reviewed = line_str[:intEnd]
-            if len(reviewed) == 0:
-                reviewed = 0
+    if page_not_found == 0:
+        html_element = soup.findAll('h1')
+        text = 'h1'
+        for i, line in enumerate(html_element):
+                intStart = str(line).find('data-test="brand-name">') + len('data-test="brand-name">')
+                line_str = str(line)
+                line_str = line_str[intStart:]
+                intEnd = line_str.find('</')
+                entry_name = line_str[:intEnd]
+                if len(entry_name) == 0:
+                    entry_name = '-'            
+                #print(entry_name)
+
+        html_element = soup.findAll('p')
+        text = 'cuisine-string white'
+        for i, line in enumerate(html_element):
+            if str(line).find(text) > 1:
+                intStart = str(line).find('"brand-cusine">') + len('"brand-cusine">')
+                line_str = str(line)
+                line_str = line_str[intStart:]
+                intEnd = line_str.find('</p')
+                category_name = line_str[:intEnd]
+                if len(category_name) == 0:
+                    category_name = '-'
+                #print(category_name)
+
+        html_element = soup.findAll('div')
+        for i, line in enumerate(html_element):
+            if i == 35:
+                intStart = str(line).find('number">') + len('number">')
+                line_str = str(line)
+                line_str = line_str[intStart:]
+                intEnd = line_str.find('</div>')
+                rating = line_str[:intEnd]
+                if len(rating) == 0:
+                    rating = 0
+                break
+
+        html_element = soup.findAll('div')
+        text = '"brand-total-ratings">( '
+        for i, line in enumerate(html_element):
+            if str(line).find(text) > 1:
+                intStart = str(line).find('"brand-total-ratings">( ') + len('"brand-total-ratings">( ')
+                line_str = str(line)
+                line_str = line_str[intStart:]
+                intEnd = line_str.find(' Ratings')
+                rated = line_str[:intEnd]
+                rated = line_str[:intEnd]
+            else:
+                rated = 0
             break
 
-    html_element = soup.findAll('div')
-    text = 'markdown-rich-text-block'
-    for i, line in enumerate(html_element):
-        if str(line).find(text) > 1:
-            intStart = str(line).find('block"><p>') + len('block"><p>')
-            line_str = str(line)
-            line_str = line_str[intStart:]
-            intEnd = line_str.find('</div>')
-            desc_msg = line_str[:intEnd]
-            desc_msg = desc_msg.replace('<p>','')
-            desc_msg = desc_msg.replace('</p>','')
-            # print(desc_msg)
-            break
+        html_element = soup.findAll('span')
+        text = 'brand-total-reviews">'
+        for i, line in enumerate(html_element):
+            if str(line).find(text) > 1:
+                intStart = str(line).find('reviews">') + len('reviews">')
+                line_str = str(line)
+                line_str = line_str[intStart:]
+                intEnd = line_str.find(' Reviews')
+                reviewed = line_str[:intEnd]
+                if len(reviewed) == 0:
+                    reviewed = 0
+                break
 
-    print(url)
+        html_element = soup.findAll('div')
+        text = 'markdown-rich-text-block'
+        for i, line in enumerate(html_element):
+            if str(line).find(text) > 1:
+                intStart = str(line).find('block"><p>') + len('block"><p>')
+                line_str = str(line)
+                line_str = line_str[intStart:]
+                intEnd = line_str.find('</div>')
+                desc_msg = line_str[:intEnd]
+                desc_msg = desc_msg.replace('<p>','')
+                desc_msg = desc_msg.replace('</p>','')
+                # print(desc_msg)
+                break
 
-    insert_command = "Insert Into talabat_restaurants (entry_name, category, rating, rated, reviewed, descriptive_message, link_id) values ("
-    insert_command += "'" + entry_name.replace("'","") + "', '" + category_name.replace("'","") + "', " + str(rating) + ", " + str(rated) + ", " + str(reviewed) + ", '" + (desc_msg.replace("'",'')).replace('','') + "', " + str(row[0]) + ")"
+        print(str(row[0]) + ' ' + url)
+
+        
+        insert_command = "Insert Into talabat_restaurants (entry_name, category, rating, rated, reviewed, descriptive_message, link_id) values ("
+        insert_command += "'" + entry_name.replace("'","") + "', '" + category_name.replace("'","") + "', " + str(rating) + ", " + str(rated) + ", " + str(reviewed) + ", '" + (desc_msg.replace("'",'')).replace('','') + "', " + str(row[0]) + ")"
+        
+        # print(insert_command)
+        cursor.execute(insert_command)
+        conn.commit()
+
+        update_command = 'Update talabat_restaurants_urls Set [done] = 1, [page_active] = 1 where id = ' + str(row[0])
+        cursor.execute(update_command)
+        conn.commit()
     
-    # print(insert_command)
-    cursor.execute(insert_command)
-    conn.commit()
 
-    update_command = 'Update talabat_restaurants_urls Set [done] = 1 where id = ' + str(row[0])
-    cursor.execute(update_command)
-    conn.commit()
 
 print('\n' +str(c) + ' records inserted!')
 
